9096169307d7468e872033e5d1d0ac04fed3d9c1
Wenuka19 <89344987+Wenuka19@users.noreply.github.com>
2023-04-18 16:37:25 +0530

    FRONTEND : sign-up page➕flash messages

--- src/auth.py
+++ src/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['POST','GET'])
 def login():
     return render_template('login.html')
 
@@ -10,6 +10,22 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['POST','GET'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) <4:
+            flash('Email must be greater than 4 characters',category='error')
+        elif len(firstName) <=2 :
+            flash('First name must be greater than 2 characters',category='error')
+        elif password1 != password2:
+            flash('Passwords do not match',category='error')
+        elif len(password1) < 7:
+            flash('Password must be grater than 6 characters',category='error')
+        else:
+            flash('Account created successfully',category='success')
     return render_template('sign_up.html')
